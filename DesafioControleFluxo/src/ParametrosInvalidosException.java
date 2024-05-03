e46dcf6a113190c0242306ec0bde14214cfcc342
daniloqr <daniloqr1990@gmail.com>
2024-05-03 14:58:00 -0300

    ajuste no código

--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,3 +1,5 @@
-public class ParametrosInvalidosException extends Exception {
-
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O primeiro número não pode ser maior que o segundo.");
+    }
 }
--- DesafioControleFluxo/src/contador.java
+++ DesafioControleFluxo/src/contador.java
@@ -11,23 +11,31 @@ public class contador {
         int numero2 = entrada.nextInt();
 
         try {
-            contar(numero1, numero2);
-        } catch (ParametrosInvalidosException e) {
-            System.out.println("o primeiro número precisa ser maior do que o segundo número.");
+            verificarNumeros(numero1, numero2);
+            int contagem = numero2 - numero1;
+            outraFuncao(contagem);
+        }
+
+        catch (ParametrosInvalidosException e) {
+            System.out.println("Ocorreu uma exceção: " + e.getMessage());
         }
 
     }
 
-    static int contar(int numero1, int numero2) throws ParametrosInvalidosException {
+    public static void verificarNumeros(int numero1, int numero2) throws ParametrosInvalidosException {
         if (numero1 > numero2) {
             throw new ParametrosInvalidosException();
         }
-        int contagem = numero1 - numero2;
+        System.out.println("Os números são válidos.");
 
-        for (int i = 1; i < contagem; i++) {
+    }
+
+    public static void outraFuncao(int contagem) {
+        for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
 
         }
 
     }
+
 }
